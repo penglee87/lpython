c0a15cbd51891df09d09b173f9b30bb9640bd323
penglee87 <penglee87@163.com>
2024-04-20 11:36:53 +0800

    Update do_flask_mail1.py

--- do_flask_mail1.py
+++ do_flask_mail1.py
@@ -1,25 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from flask import Flask
-from flask_mail import Mail
-from flask_mail import Message
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
-app.config['MAIL_SERVER'] = 'smtp.163.com'
-app.config['MAIL_PORT'] = 25
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'penglee87@163.com'
-app.config['MAIL_PASSWORD'] = '******'
+
+app.config.update(
+    MAIL_SERVER='smtp.qq.com',
+    MAIL_PORT=465,
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME='380517767@qq.com',
+    MAIL_PASSWORD='******',  # 使用QQ邮箱的授权码
+)
 
 mail = Mail(app)
 
-
-msg = Message("Hello",
-              sender="penglee87@163.com",
-              recipients=["lipeng@163.com"])
-msg.body = "testing"
-msg.html = "<b>testing</b>"
-
-
-with app.app_context():
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
     mail.send(msg)
+
+@app.route('/send_email')
+def send_email_route():
+    subject = "flask Email Title"
+    sender = "380517767@qq.com"
+    recipients = ["penglee87@163.com"]
+    text_body = "this is a flask test email"
+    html_body = "<h1>this is a flask test email</h1>"
+
+    send_email(subject, sender, recipients, text_body, html_body)
+
+    return "email send success"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+'''
+运行后,打开浏览器访问 http://127.0.0.1:5000/send_email
+'''
